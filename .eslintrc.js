3301aa4846c109de2df674bbedb469e855778031
Jules Paris <jules.paris@360learning.com>
2022-12-30 12:43:53 +0100

    chore: Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
@@ -17,4 +18,23 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  overrides: [
+    {
+      files: ["*.vue"],
+      rules: {
+        "vue/attributes-order": [
+          "error",
+          {
+            alphabetical: true,
+          },
+        ],
+        "vue/require-default-prop": "off",
+        "vue/sort-keys": ["error", "asc"],
+      },
+    },
+    {
+      files: ["*.stories.ts"],
+      rules: {},
+    },
+  ],
 };
